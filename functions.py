45808bce4fcec7cd873d20a6792432c3b6a21e96
Oksana911 <rosa111@mail.ru>
2022-08-24 12:51:51 +0700

    upd: typing

--- functions.py
+++ functions.py
@@ -1,34 +1,35 @@
 import os
 import re
+from typing import Iterable
+
+from werkzeug.exceptions import BadRequest
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
 
-def do_cmd(cmd: str, value: str, data: list[str]) -> list[str]:
+def do_cmd(cmd: str, value: str, data: Iterable) -> Iterable:
     """ Обрабатывает запрос с командами """
     if cmd == 'filter':
-        result = filter(lambda line: value in line, data)
-        return list(result)
+        result = list(filter(lambda line: value in line, data))
     elif cmd == 'map':
-        result = map(lambda line: line.split()[int(value)], data)
-        return list(result)
+        result = list(map(lambda line: line.split()[int(value)], data))
     elif cmd == 'unique':
-        result = set(data)
-        return list(result)
+        result = list(set(data))
     elif cmd == 'sort':
         reverse = (value == 'desc')
         result = sorted(data, reverse=reverse)
-        return result
     elif cmd == 'limit':
         result = data[:int(value)]
-        return result
     elif cmd == 'regex':
         regex = re.compile(value)
-        return list(filter(lambda v: regex.search(v), data))
+        result = list(filter(lambda v: regex.search(v), data))
+    else:
+        raise BadRequest
+    return result
 
 
-def do_query(params: dict) -> list[str]:
+def do_query(params: dict) -> Iterable:
     """ Считывает файл и возвращает обработанный результат """
     with open(os.path.join(DATA_DIR, params['file_name'])) as f:
         file_data = f.readlines()  # f.read().split('\n')
